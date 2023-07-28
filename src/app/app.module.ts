6c73a89de0a5c7247355e900228ff968e591f750
malak895 <malak.hammami263@gmail.com>
2023-07-28 15:24:11 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChatComponent } from './chat/chat.component';
+
 import { ChatbotAiComponent } from './chatbot-ai/chatbot-ai.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -10,7 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent,
+
     ChatbotAiComponent,
   ],
   imports: [
